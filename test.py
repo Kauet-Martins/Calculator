aa9fb7cef909f9efb904f736c99076385c127708
Kauet-Martins <kauetdamotta@gmail.com>
2024-07-31 21:03:55 -0300

    Interface Almost Finished, Only the Logic is Missing

--- test.py
+++ test.py
@@ -1,28 +1,47 @@
 import tkinter as tk
 
-def adicionar_numero(num,):
-    valor_atual = entry.get
+def add_number():
 
-    novo_valor = valor_atual + str(num)
+    entrada.insert(0, 1)
 
-    entry.delete(0, tk.END)
-
-    entry.insert(tk.END, novo_valor)
+    
 
 root = tk.Tk()
 
-root.title("Calculator App")
 
-root.geometry('300x400')
+root.geometry("300x275")
 
-frm = tk.Frame(root)
+entrada = tk.Entry(root, width=20,justify="center", font=("Arial", 20))
+entrada.place(x=0, y=5)
 
-entry = tk.Entry(root, width=16, font=('Arial', 24), bd=7, insertwidth=2, justify='right')
+botao_1 = tk.Button(root, text=1, width=10, command=add_number)
+botao_1.place(x=20, y=50)
 
-entry.grid(row=0, column=0, columnspan=4)
+botao_2 = tk.Button(root, text=2, width=10)
+botao_2.place(x=110, y=50)
 
-botao_1 = tk.Button(root, text='1', padx=20, pady=20, command=lambda: adicionar_numero(1))
+botao_3 = tk.Button(root, text=3, width=10)
+botao_3.place(x=200, y=50)
 
-botao_1.grid(row=1, column=0)
+botao_4 = tk.Button(root, text=4, width=10)
+botao_4.place(x=20, y=90)
+
+botao_5 = tk.Button(root, text=5, width=10)
+botao_5.place(x=110, y=90)
+
+botao_6 = tk.Button(root, text=6, width=10)
+botao_6.place(x=200, y=90)
+
+botao_7 = tk.Button(root, text=7, width=10)
+botao_7.place(x=20, y=130)
+
+botao_8 = tk.Button(root, text=8, width=10)
+botao_8.place(x=110, y=130)
+
+botao_9 = tk.Button(root, text=9, width=10)
+botao_9.place(x=200, y=130)
+
+botao_0 = tk.Button(root, text=0, width=10)
+botao_0.place(x=110, y=170)
 
 root.mainloop()
